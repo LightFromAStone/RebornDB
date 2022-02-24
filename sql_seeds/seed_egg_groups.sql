e3dae623614d45ca173d75532f1bcc96bfe220e9
LightFromAStone <github@lightfromastone.me>
2022-02-23 21:19:01 -0700

    updated seed files

---
+++ sql_seeds/seed_egg_groups.sql
@@ -0,0 +1,17 @@
+INSERT INTO egg_groups (egg_group_name)
+VALUES
+(Monster),
+(Humanlike),
+(Water1),
+(Water3),
+(Bug),
+(Mineral),
+(Flying),
+(Amorphous),
+(Field),
+(Water2),
+(Fairy),
+(Ditto),
+(Grass),
+(Dragon),
+(Undiscovered);
