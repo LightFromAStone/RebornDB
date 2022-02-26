b954526b5cbb395ec993fde867dd036ea080b5b1
LightFromAStone <github@lightfromastone.me>
2022-02-26 12:03:13 -0700

    added tables to handle items

--- sql_seeds/create_tables.sql
+++ sql_seeds/create_tables.sql
@@ -1,15 +1,43 @@
-CREATE TYPE EXP AS ENUM ('Erratic', 'Fast', 'Medium Fast', 'Medium Slow', 'Slow', 'Fluctuating');
+CREATE TYPE EXPGROWTH AS ENUM ('Erratic', 'Fast', 'Medium Fast', 'Medium Slow', 'Slow', 'Fluctuating');
+CREATE TYPE POCKET AS ENUM ('Items', 'Medicine', 'Poke Balls', 'TMs & HMs', 'Berries', 'Mail', 'Battle Items', 'Key Items');
 
 CREATE TABLE pokemon_type (
    pokemon_type_id SERIAL PRIMARY KEY,
    pokemon_type_name VARCHAR(20)
 );
 
+CREATE TABLE in_battle_use (
+   in_battle_use_id SERIAL PRIMARY KEY,
+   useability_definition VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE out_battle_use (
+   out_battle_use_id SERIAL PRIMARY KEY,
+   useability_definition VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE special_items_use (
+   special_items_use_id SERIAL PRIMARY Key,
+   useability_definition VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE game_items (
+   game_item_id SERIAL PRIMARY KEY,
+   game_item_text_id VARCHAR(30) NOT NULL,
+   game_item_name VARCHAR(30) NOT NULL,
+   bag_pocket POCKET NOT NULL,
+   game_item_price INT NOT NULL,
+   game_item_description VARCHAR(200) NOT NULL,
+   in_battle_use_id INT NOT NULL REFERENCES in_battle_use(in_battle_use_id),
+   out_battle_use_id INT NOT NULL REFERENCES out_battle_use(out_battle_use_id),
+   special_items_use_id INT NOT NULL REFERENCES special_items_use(special_items_use_id)
+);
+
 CREATE TABLE pokemon_base (
    pokemon_base_id SERIAL PRIMARY KEY,
    pokemon_name VARCHAR(50) NOT NULL,
    gender_rate NUMERIC(4, 3) NOT NULL,
-   growth_type EXP NOT NULL,
+   growth_type EXPGROWTH NOT NULL,
    catch_rate INT NOT NULL,
    hatch_steps INT NOT NULL
 );
@@ -20,6 +48,14 @@ CREATE TABLE pokemon (
    pokemon_text_id VARCHAR(50) NOT NULL,
    pokemon_type_1 INT NOT NULL REFERENCES type(pokemon_type_id),
    pokemon_type_2 INT REFERENCES pokemon_type(pokemon_type_id),
+   wild_item_id
+);
+
+CREATE TABLE wild_held_items (
+   wild_held_item_id SERIAL PRIMARY KEY,
+   game_item_id INT NOT NULL REFERENCES game_items(game_item_id),
+   pokemon_id INT NOT NULL REFERENCES pokemon(pokemon_id),
+   held_chance NUMERIC(3,2) NOT NULL
 );
 
 CREATE TABLE effort_points (
@@ -92,6 +128,12 @@ CREATE TABLE pokemon_moves (
    pokemon_id INT NOT NULL REFERENCES pokemon(pokemon_id),
    move_id INT NOT NULL REFERENCES moves(move_id),
    learn_method VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE technical_machines (
+   technical_machine_id SERIAL PRIMARY KEY,
+   game_item_id INT NOT NULL REFERENCES game_items(game_item_id),
+   move_taught INT NOT NULL REFERENCES moves(move_id)
 );
 
 CREATE TABLE flags (
